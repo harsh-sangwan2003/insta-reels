570f9f4280fedc7965a2472317c0771bfcd04005
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-09-14 10:47:01 +0530

    posts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../Context/AuthContext'
 import { database } from '../firebase'
 import UploadFile from './UploadFile'
+import Posts from './Posts';
 
 function Feed() {
 
@@ -18,13 +19,14 @@ function Feed() {
     return (
         <>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                
+
                 <div className="comp" style={{ width: '50%' }}>
                     <h1>Welcome to feed</h1>
                     <button onClick={logout}>Log out</button>
                 </div>
 
                 <UploadFile user={userData} />
+                <Posts userData={userData} />
             </div>
         </>
     )
